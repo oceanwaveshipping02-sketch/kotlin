dbb0ae9413c3875300f9e38cb7121a49ef4cab6b
Dmitrii Gridin <dmitry.gridin@jetbrains.com>
2025-08-07 15:54:40 +0200

    [Analysis API FIR] use `<unused var>` name instead of `_` for consistency
    ^KT-75318

--- analysis/analysis-api-fir/src/org/jetbrains/kotlin/analysis/api/fir/psiUtils.kt
+++ analysis/analysis-api-fir/src/org/jetbrains/kotlin/analysis/api/fir/psiUtils.kt
@@ -108,7 +108,7 @@ internal val KtDestructuringDeclarationEntry.entryName: Name
 internal val KtParameter.parameterName: Name
     get() = when {
         destructuringDeclaration != null -> SpecialNames.DESTRUCT
-        isSingleUnderscore -> SpecialNames.UNDERSCORE_FOR_UNUSED_VAR
+        name == "_" -> SpecialNames.UNDERSCORE_FOR_UNUSED_VAR
         else -> nameAsSafeName
     }
 
