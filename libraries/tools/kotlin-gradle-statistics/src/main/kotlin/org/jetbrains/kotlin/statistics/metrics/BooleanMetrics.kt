aa9e58137a4f1f89263e9a3d973fbcba02491aee
Andrey Yastrebov <andrey.yastrebov@jetbrains.com>
2025-08-14 15:59:07 +0200

    fixup! [Gradle] Add FUS metrics to track binary's debuggable & optimized flags

--- libraries/tools/kotlin-gradle-statistics/src/main/kotlin/org/jetbrains/kotlin/statistics/metrics/BooleanMetrics.kt
+++ libraries/tools/kotlin-gradle-statistics/src/main/kotlin/org/jetbrains/kotlin/statistics/metrics/BooleanMetrics.kt
@@ -127,6 +127,6 @@ enum class BooleanMetrics(val type: BooleanOverridePolicy, val anonymization: Bo
     ;
 
     companion object {
-        const val VERSION = 18
+        const val VERSION = 19
     }
 }
