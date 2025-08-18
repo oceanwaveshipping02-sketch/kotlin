080a9239433b21d0500731dff32a7abf2434ed88
Dmitrii Gridin <dmitry.gridin@jetbrains.com>
2025-08-18 15:56:36 +0200

    [LL] AbstractLLStubBasedTest: reuse stubs from real KtFiles
    So the test infrastructure wouldn't affect the resolution
    (e.g., jumps to the ast-based file)
    
    ^KT-79798
    ^KT-78949 Fixed

--- analysis/low-level-api-fir/testFixtures/org/jetbrains/kotlin/analysis/low/level/api/fir/AbstractLLStubBasedTest.kt
+++ analysis/low-level-api-fir/testFixtures/org/jetbrains/kotlin/analysis/low/level/api/fir/AbstractLLStubBasedTest.kt
@@ -5,7 +5,11 @@
 
 package org.jetbrains.kotlin.analysis.low.level.api.fir
 
+import com.intellij.openapi.util.Disposer
+import com.intellij.psi.AbstractFileViewProvider
+import com.intellij.psi.impl.PsiManagerEx
 import org.jetbrains.kotlin.analysis.low.level.api.fir.AbstractLLStubBasedTest.Companion.computeAstLoadingAware
+import org.jetbrains.kotlin.analysis.low.level.api.fir.api.LLResolutionFacade
 import org.jetbrains.kotlin.analysis.test.framework.base.AbstractAnalysisApiBasedTest
 import org.jetbrains.kotlin.analysis.test.framework.projectStructure.KtTestModule
 import org.jetbrains.kotlin.fir.builder.AbstractRawFirBuilderLazyBodiesByStubTest
@@ -13,18 +17,20 @@ import org.jetbrains.kotlin.psi.KtFile
 import org.jetbrains.kotlin.test.directives.model.DirectivesContainer
 import org.jetbrains.kotlin.test.directives.model.SimpleDirectivesContainer
 import org.jetbrains.kotlin.test.services.TestServices
+import org.jetbrains.kotlin.test.services.assertions
 import org.jetbrains.kotlin.test.services.moduleStructure
 
 /**
  * This test case allows testing differences between stub-based and AST-based files.
  *
- * The entry point for tests is [doTest].
+ * The entry points for tests are [doStubBasedPart] and [doAstBasedPart].
  *
+ * @see withAstLoadingAssertion
  * @see computeAstLoadingAware
  * @see com.intellij.psi.impl.source.PsiFileImpl.loadTreeElement
  * @see com.intellij.psi.impl.PsiManagerEx.isAssertOnFileLoading
  */
-abstract class AbstractLLStubBasedTest : AbstractAnalysisApiBasedTest() {
+abstract class AbstractLLStubBasedTest<StubBasedOutput> : AbstractAnalysisApiBasedTest() {
     override val additionalDirectives: List<DirectivesContainer>
         get() = super.additionalDirectives + listOf(Directives)
 
@@ -41,18 +47,58 @@ abstract class AbstractLLStubBasedTest : AbstractAnalysisApiBasedTest() {
     override fun doTestByMainFile(mainFile: KtFile, mainModule: KtTestModule, testServices: TestServices) {
         if (Directives.IGNORE_TREE_ACCESS in testServices.moduleStructure.allDirectives) return
 
-        val stubBasedFile = AbstractRawFirBuilderLazyBodiesByStubTest.createKtFile(mainFile, disposable)
-        doTest(
-            astBasedFile = mainFile,
-            stubBasedFile = stubBasedFile,
-            mainModule = mainModule,
-            testServices = testServices,
-        )
+        val viewProvider = mainFile.viewProvider as AbstractFileViewProvider
+        val myPhysicalField = AbstractFileViewProvider::class.java.getDeclaredField("myPhysical").apply { isAccessible = true }
+
+        // A hack to enable AST loading assertion and allow stub requests
+        myPhysicalField.set(viewProvider, true)
+
+        mainFile.setTreeElementPointer(null)
+        testServices.assertions.assertNotNull(mainFile.stub) { "Stub should be present for unloaded file" }
+
+        val output = withAstLoadingAssertion(mainFile) {
+            withResolutionFacade(mainFile) { facade ->
+                context(facade) {
+                    doStubBasedPart(mainFile, mainModule, testServices)
+                }
+            }
+        }
+
+        // We need to clear caches to ensure that we will not load the file from the cache
+        clearCaches(mainFile.project)
+        mainFile.calcTreeElement()
+        testServices.assertions.assertTrue(mainFile.stub == null) { "Stub shouldn't be present for loaded file" }
+
+        withResolutionFacade(mainFile) { facade ->
+            context(facade) {
+                doAstBasedPart(output, mainFile, mainModule, testServices)
+            }
+        }
     }
 
-    abstract fun doTest(
-        astBasedFile: KtFile,
+    protected fun <T> withAstLoadingAssertion(file: KtFile, action: () -> T): T {
+        val virtualFile = file.virtualFile
+        val disposable = Disposer.newDisposable("AST loading assertion")
+        (file.manager as PsiManagerEx).setAssertOnFileLoadingFilter({ it == virtualFile }, disposable)
+        return try {
+            action()
+        } finally {
+            Disposer.dispose(disposable)
+        }
+    }
+
+    context(facade: LLResolutionFacade)
+    abstract fun doStubBasedPart(
         stubBasedFile: KtFile,
+        mainModule: KtTestModule,
+        testServices: TestServices,
+    ): StubBasedOutput
+
+    /** @param stubBasedOutput the output of [doStubBasedPart] */
+    context(facade: LLResolutionFacade)
+    abstract fun doAstBasedPart(
+        stubBasedOutput: StubBasedOutput,
+        astBasedFile: KtFile,
         mainModule: KtTestModule,
         testServices: TestServices,
     )
