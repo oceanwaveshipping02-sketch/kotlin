5d4ce1ef003d901eebd7e27b84ab38781cd41c7a
Dmitrii Gridin <dmitry.gridin@jetbrains.com>
2025-08-22 17:06:01 +0200

    [standalone] improve stub index performance
    It is more performant to retrieve the stub tree by `greenStubTree`
    API since it might not trigger the AST loading
    
    ^KT-79798

--- analysis/analysis-api-standalone/analysis-api-standalone-base/src/org/jetbrains/kotlin/analysis/api/standalone/base/declarations/KotlinStandaloneDeclarationProvider.kt
+++ analysis/analysis-api-standalone/analysis-api-standalone-base/src/org/jetbrains/kotlin/analysis/api/standalone/base/declarations/KotlinStandaloneDeclarationProvider.kt
@@ -519,7 +519,7 @@ private fun computeIndex(
         // Stub calculation
         if (indexableFile.isShared && cacheService != null) {
             val stub = cacheService.getOrBuildStub(virtualFile) {
-                ktFile.calcStubTree().root as KotlinFileStubImpl
+                ktFile.forcedStub
             }
 
             if (stub.psi != ktFile) {
@@ -529,11 +529,9 @@ private fun computeIndex(
                 // A hack to avoid costly stub builder execution
                 setStubTreeMethod.invoke(ktFile, clonedStub)
             }
-        } else {
-            ktFile.calcStubTree()
         }
 
-        val stub = ktFile.greenStub as KotlinFileStubImpl
+        val stub = ktFile.forcedStub
         index.indexStubRecursively(stub)
     }
 
@@ -622,6 +620,15 @@ private fun computeIndex(
         },
     )
 }
+
+/**
+ * [PsiFileImpl.getGreenStubTree] is cheaper if it is available since it doesn't require computing the AST tree
+ */
+private val KtFile.forcedStub: KotlinFileStubImpl
+    get() {
+        val stubTree = greenStubTree ?: calcStubTree()
+        return stubTree.root as KotlinFileStubImpl
+    }
 
 /**
  * Test application service to store stubs of shared between tests libraries.
